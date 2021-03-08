6aeaf13edfe79fb6a8ffdea52b3078d941d2fe5d
Gari Kogan <garikf1h@gmail.com>
2021-03-08 15:04:02 +0200

    updated column relationship_stat

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ mysql = MySQL(app)
 def first():
     if request.method == "POST":
         cur = mysql.connection.cursor()
-        # cur.execute("CREATE TABLE EXAM1(ID INT PRIMARY KEY not null , email VARCHAR(255),   full_name VARCHAR(255), activity_level VARCHAR(2), number_of_child VARCHAR(2))")
+        #cur.execute("CREATE TABLE EXAM1(ID INT PRIMARY KEY not null , email VARCHAR(255),   full_name VARCHAR(255), activity_level VARCHAR(2), number_of_child VARCHAR(2))")
         full_name = request.form['full_name']
         email = request.form['email']
         id1 = request.form['ID_N']
         number_of_child = request.form['select-number']
         activity_level = request.form['active']
+        relationship_stat = request.form.getlist('selector')
         print(full_name, email, id1, number_of_child, activity_level)
-        cur.execute("INSERT INTO EXAM1(ID, email, full_name, activity_level, number_of_child) VALUES(%s, %s,%s,%s,%s)", (id1, email, full_name, activity_level, number_of_child))
+        cur.execute("INSERT INTO EXAM1(ID, email, full_name, activity_level, number_of_child, relationship_status) VALUES(%s, %s,%s,%s,%s, %s)", (id1, email, full_name, activity_level, number_of_child,relationship_stat[0]))
         mysql.connection.commit()
 
         return redirect("/")

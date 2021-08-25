3cb6caabf452c2f4db54bab5a6da5908d37a7f04
idouziely <63237801+idouziely@users.noreply.github.com>
2021-08-25 19:34:30 +0300

    Added cards for form

---
+++ frontend/src/pages/recommendationpage/Component/Cards/RestCard.jsx
@@ -0,0 +1,39 @@
+import { Card, Feed, Segment } from "semantic-ui-react";
+import React from "react";
+
+export const RestCard = (props) => {
+  console.log(props);
+  const { rest } = props.props;
+  return (
+    <Card
+      style={{
+        position: "absolute",
+        textAlign: "right",
+        top: "250px",
+      }}
+      key={rest.name}
+    >
+      <Card.Content style={{ textAlign: "right" }}>
+        <Feed.Event>
+          <Feed.Content>
+            <Feed.Date style={{ textAlign: "right" }} content=":מסעדה" />
+            <div style={{ textAlign: "right" }}>
+              {" "}
+              <b>{rest.name}</b>{" "}
+            </div>
+            <div style={{ textAlign: "right" }}>
+              {" "}
+              {rest.rating} <b>:ציון המסעדה</b>{" "}
+            </div>
+            <div style={{ textAlign: "right" }}>
+              {" "}
+              {rest.vicinity} <b>כתובת</b>{" "}
+            </div>
+          </Feed.Content>
+        </Feed.Event>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default { RestCard };

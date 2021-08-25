1c18c42144454c9c0877afab9ff86fb6da670390
idouziely <63237801+idouziely@users.noreply.github.com>
2021-08-25 19:35:33 +0300

    Added cards for form (#22)

--- frontend/src/pages/Main/Component/Map/facade.js
+++ frontend/src/pages/Main/Component/Map/facade.js
@@ -1,127 +1,166 @@
 import React, { useState, useEffect } from "react";
-import { cloneDeep } from 'lodash';
+import { cloneDeep } from "lodash";
 import axios from "axios";
-import {Divider} from "@material-ui/core";
-import {Button} from "semantic-ui-react";
-import {Marker} from "react-google-maps";
+import { Divider } from "@material-ui/core";
+import { Button } from "semantic-ui-react";
+import { Marker } from "react-google-maps";
 import Res from "./Markers/Res.svg";
 
 export const useMapFacade = (props) => {
-    const {searchParams, openSidebar, setTripExtraData, restParams, fullTrip, setFullTrip, hotelParams, setSidebarData, sidebarData} = props
-    const [data, setData] = useState([]);
-    const [restData, setRestData] = useState([]);
-    const [hotelData, setHotelData] = useState([]);
-    const [tripToShow, setTripToShow] = useState(undefined);
-    const [restToShow, setRestToShow] = useState(undefined);
-    const [hotelToShow, setHotelToShow] = useState(undefined);
-    const [selectedTrip, setSelectedTrip] = useState(undefined);
-    const [showPopUp, setShowPopUp] = useState(false);
+  const {
+    searchParams,
+    openSidebar,
+    setTripExtraData,
+    restParams,
+    fullTrip,
+    setFullTrip,
+    hotelParams,
+    setSidebarData,
+    sidebarData,
+  } = props;
+  const [data, setData] = useState([]);
+  const [restData, setRestData] = useState([]);
+  const [hotelData, setHotelData] = useState([]);
+  const [tripToShow, setTripToShow] = useState(undefined);
+  const [restToShow, setRestToShow] = useState(undefined);
+  const [hotelToShow, setHotelToShow] = useState(undefined);
+  const [selectedTrip, setSelectedTrip] = useState(undefined);
+  const [showPopUp, setShowPopUp] = useState(false);
 
+  const addRest = (rest) => {
+    const clone = cloneDeep(fullTrip);
+    clone.rest = rest;
+    setFullTrip(clone);
+  };
 
-    const addRest = (rest) => {
-        const clone = cloneDeep(fullTrip);
-        clone.rest = rest;
-        setFullTrip(clone);
+  const addHotel = (hotel) => {
+    const clone = cloneDeep(fullTrip);
+    clone.hotel = hotel;
+    setFullTrip(clone);
+  };
+
+  useEffect(() => {
+    const clone = cloneDeep(fullTrip);
+    clone.trip = selectedTrip;
+    setFullTrip(clone);
+  }, [selectedTrip]);
+
+  useEffect(() => {
+    const newSideData = {
+      hotelData: [],
+      restData: [],
+      tripData: data,
     };
+    setSidebarData(newSideData);
+  }, [data]);
 
-    const addHotel = (hotel) => {
-        const clone = cloneDeep(fullTrip);
-        clone.hotel = hotel;
-        setFullTrip(clone);
-    };
+  useEffect(() => {
+    if (restData !== []) {
+      const newSideData = {
+        hotelData: [],
+        restData,
+        tripData: [],
+      };
+      setSidebarData(newSideData);
+    }
+  }, [restData]);
 
-    useEffect(() => {
-        const newSideData = {
-            hotelData:[],
-            restData:[],
-            tripData:data,
-        };
-        setSidebarData(newSideData);
-    }, [data]);
+  useEffect(() => {
+    if (hotelData !== []) {
+      const newSideData = {
+        hotelData: hotelData,
+        restData: [],
+        tripData: [],
+      };
+      setSidebarData(newSideData);
+    }
+  }, [hotelData]);
 
-    useEffect(() => {
-        if (restData !== []) {
-            const newSideData = {
-            hotelData:[],
-            restData,
-            tripData:[],
-        };
-        setSidebarData(newSideData);
-        }
+  useEffect(() => {
+    setHotelData([]);
+    setRestData([]);
+    setHotelToShow(undefined);
+    setRestToShow(undefined);
+  }, [data]);
 
-    }, [restData]);
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/map", { type: "Trip", data: searchParams })
+      .then((response) => {
+        setData(response.data.data);
+        setSelectedTrip(undefined);
+        setFullTrip({
+          trip: undefined,
+          rest: undefined,
+          hotel: undefined,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [searchParams]);
 
-    useEffect(() => {
-        if(hotelData !== []) {
-            const newSideData = {
-            hotelData:hotelData,
-            restData:[],
-            tripData:[],
-        };
-        setSidebarData(newSideData);
-        }
-    }, [hotelData]);
-
-    useEffect(() => {
-        setHotelData([]);
-        setRestData([]);
-        setHotelToShow(undefined);
-        setRestToShow(undefined);
-    }, [data])
-
-      useEffect(()=>{
-          axios.post('http://localhost:5000/map', {type: 'Trip', data:searchParams}).then(response => {
-          setData(response.data.data);
-        }).catch(error => {
-            
-          console.log(error);
+  useEffect(() => {
+    if (selectedTrip !== undefined) {
+      const dataTrip = {
+        coordinates: {
+          y: selectedTrip.Starting_point_y,
+          x: selectedTrip.Starting_point_x,
+        },
+        restInfo: restParams,
+      };
+      axios
+        .post("http://localhost:5000/trip", { type: "rest", data: dataTrip })
+        .then((response) => {
+          setRestData(response.data);
+          addRest(undefined);
         })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [restParams]);
 
-      }, [searchParams]);
+  useEffect(() => {
+    if (selectedTrip !== undefined) {
+      const dataTrip = {
+        coordinates: {
+          y: selectedTrip.Starting_point_y,
+          x: selectedTrip.Starting_point_x,
+        },
+        hotelInfo: hotelParams,
+      };
+      axios
+        .post("http://localhost:5000/trip", { type: "hotel", data: dataTrip })
+        .then((response) => {
+          setSidebarData(response.data);
+          setHotelData(response.data);
+          addHotel(undefined);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [hotelParams]);
 
-       useEffect (() => {
-           if (selectedTrip !== undefined) {
-               const dataTrip = {coordinates: {y : selectedTrip.Starting_point_y, x: selectedTrip.Starting_point_x}, restInfo: restParams}
-                  axios.post('http://localhost:5000/trip', {type: 'rest', data: dataTrip}).then(response => {
-                  // setSelectedTrip(undefined);
-                  setRestData(response.data);
-                }).catch((error) => {
-            console.log(error);
-           })
-           }
-       }, [restParams] );
-
-       useEffect (() => {
-           if (selectedTrip !== undefined) {
-               const dataTrip = {coordinates: {y : selectedTrip.Starting_point_y, x: selectedTrip.Starting_point_x}, hotelInfo: hotelParams}
-                  axios.post('http://localhost:5000/trip', {type: 'hotel', data: dataTrip}).then(response => {
-                  //setSelectedTrip(undefined);
-                      setSidebarData(response.data);
-                  setHotelData(response.data);
-                }).catch((error) => {
-            console.log(error);
-           })
-           }
-       }, [hotelParams] );
-
-      return {
-          data,
-          setData,
-          hotelData,
-          setHotelData,
-          addRest,
-          addHotel,
-          tripToShow,
-          setTripToShow,
-          restToShow,
-          setRestToShow,
-          hotelToShow,
-          setHotelToShow,
-          restData,
-          setRestData,
-          selectedTrip,
-          setSelectedTrip,
-          setShowPopUp,
-          showPopUp,
-      }
-}
+  return {
+    data,
+    setData,
+    hotelData,
+    setHotelData,
+    addRest,
+    addHotel,
+    tripToShow,
+    setTripToShow,
+    restToShow,
+    setRestToShow,
+    hotelToShow,
+    setHotelToShow,
+    restData,
+    setRestData,
+    selectedTrip,
+    setSelectedTrip,
+    setShowPopUp,
+    showPopUp,
+  };
+};

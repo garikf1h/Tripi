800b1c9dda907ad86e5d6669aedce4e2c20dd9d3
idouziely <63237801+idouziely@users.noreply.github.com>
2021-09-01 11:30:25 +0300

    Fixed markers

--- frontend/src/pages/Main/App.jsx
+++ frontend/src/pages/Main/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import {Button, Menu, Segment, Sidebar} from "semantic-ui-react";
+import { Button, Menu, Segment, Sidebar } from "semantic-ui-react";
 import TheMap from "./Component/Map/App";
 import { TripForm } from "./Component/TripForm/App";
 import { RestaurantForm } from "./Component/RestaurantForm/App";
 import { HotelForm } from "./Component/HotelForm/App";
-import { TripCard } from "../recommendationpage/Component/Cards/TripCards";
-import { RestCard } from "../recommendationpage/Component/Cards/RestCard";
-import { HotelCard } from "../recommendationpage/Component/Cards/HotelCards";
+import { TripCard } from "./Component/Cards/TripCards";
+import { RestCard } from "./Component/Cards/RestCard";
+import { HotelCard } from "./Component/Cards/HotelCards";
 import MainMenu from "../MainMenuBar/App";
-import './styles.css'
+import "./styles.css";
 
 export const BuildTrip = () => {
   const [searchParams, updateSearchParams] = useState({
@@ -31,88 +31,116 @@ export const BuildTrip = () => {
     hotelData: [],
     restData: [],
   });
-   const ClearAll = () =>{
-       setFullTrip({rest:undefined, hotel:undefined, trip:undefined}  );
-
-   };
-   const ClearRestAndAccom = () =>{
-       setFullTrip({rest:undefined, hotel:undefined, trip:fullTrip.trip}  );
-   };
-   const ClearAccom = () =>{
-       setFullTrip({rest:fullTrip.rest, hotel:undefined, trip:fullTrip.trip}  );
-   };
+  const ClearAll = () => {
+    setFullTrip({ rest: undefined, hotel: undefined, trip: undefined });
+  };
+  const ClearRestAndAccom = () => {
+    setFullTrip({
+      rest: undefined,
+      hotel: fullTrip.hotel,
+      trip: fullTrip.trip,
+    });
+  };
+  const ClearAccom = () => {
+    setFullTrip({ rest: fullTrip.rest, hotel: undefined, trip: fullTrip.trip });
+  };
 
   return (
-    <>  <MainMenu active_page={"First"}/>
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        animation="scale down"
-        vertical
-        onHide={() => setShowSideBar(false)}
-        visible={showSidebar}
-        direction="left"
-        width="very wide"
-      >
-{/*           <SidebarComponent sidebarData={sidebarData}/> */}
-      </Sidebar>
-      <Sidebar.Pusher dimmed={showSidebar}>
-        <div
-          className="body2"
-          style={{ display: "flex", flexDirection: "row" }}
+    <>
+      {" "}
+      <MainMenu active_page={"First"} />
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar
+          as={Menu}
+          animation="scale down"
+          vertical
+          onHide={() => setShowSideBar(false)}
+          visible={showSidebar}
+          direction="left"
+          width="very wide"
         >
-          <div key="map" className="map">
-            <TheMap
-              props={{
-                searchParams,
-                setSidebarData,
-                restParams,
-                fullTrip,
-                setFullTrip,
-                hotelParams,
-                sidebarData,
-              }}
-            />
-          </div>
-          {fullTrip.trip && <div className="new_search_form">
-            {!fullTrip.hotel && <HotelForm
-              key="hotelForm"
-              callBack={setHotelParams}
-              sidebarShow={setShowSideBar}
-            />}
-            {fullTrip.hotel &&
-            (<><HotelCard props={fullTrip} />
-                 <Button className="clear_button" onClick={ClearAccom}>אפס</Button></>)
-            }
-          </div>}
-          {fullTrip.trip && <div className="new_search_form">
-            {!fullTrip.rest && <RestaurantForm
-              key="hotelForm"
-              callBack={setRestParams}
-              sidebarShow={setShowSideBar}
-            />}
-            {fullTrip.rest && (<><RestCard props={fullTrip} />
-            <Button className="clear_button" onClick={ClearRestAndAccom}>אפס</Button></>)
-
-            }
-          </div>}
-          <div className="new_search_form">
-            {!fullTrip.trip && <TripForm
-              key="trip"
-              callBack={updateSearchParams}
-              sidebarShow={setShowSideBar}/>
-            }
-            {fullTrip.trip &&
-            (<><TripCard props={fullTrip} />
-                 <Button className="clear_button" onClick={ClearAll}>אפס הכל</Button>
+          {/*           <SidebarComponent sidebarData={sidebarData}/> */}
+        </Sidebar>
+        <Sidebar.Pusher dimmed={showSidebar}>
+          <div
+            className="body2"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <div key="map" className="map">
+              <TheMap
+                props={{
+                  searchParams,
+                  setSidebarData,
+                  restParams,
+                  fullTrip,
+                  setFullTrip,
+                  hotelParams,
+                  sidebarData,
+                }}
+              />
+            </div>
+            {fullTrip.trip && (
+              <div className="new_search_form">
+                {!fullTrip.hotel && (
+                  <HotelForm
+                    key="hotelForm"
+                    callBack={setHotelParams}
+                    sidebarShow={setShowSideBar}
+                  />
+                )}
+                {fullTrip.hotel && (
+                  <>
+                    <HotelCard props={fullTrip} />
+                    <Button className="clear_button" onClick={ClearAccom}>
+                      אפס
+                    </Button>
+                  </>
+                )}
+              </div>
+            )}
+            {fullTrip.trip && (
+              <div className="new_search_form">
+                {!fullTrip.rest && (
+                  <RestaurantForm
+                    key="hotelForm"
+                    callBack={setRestParams}
+                    sidebarShow={setShowSideBar}
+                  />
+                )}
+                {fullTrip.rest && (
+                  <>
+                    <RestCard props={fullTrip} />
+                    <Button
+                      className="clear_button"
+                      onClick={ClearRestAndAccom}
+                    >
+                      אפס
+                    </Button>
+                  </>
+                )}
+              </div>
+            )}
+            <div className="new_search_form">
+              {!fullTrip.trip && (
+                <TripForm
+                  key="trip"
+                  callBack={updateSearchParams}
+                  sidebarShow={setShowSideBar}
+                />
+              )}
+              {fullTrip.trip && (
+                <>
+                  <TripCard props={fullTrip} />
+                  <Button className="clear_button" onClick={ClearAll}>
+                    אפס הכל
+                  </Button>
                 </>
-                )
-            }
+              )}
+            </div>
+            <article></article>
           </div>
-          <article>
-</article>
-        </div>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable></>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </>
   );
 };

18246c03bbe386950be0736ae61e3ed4982f885b
YardenGoor1994 <80250917+YardenGoor1994@users.noreply.github.com>
2021-09-03 17:41:25 +0300

    added a home page and made it the landing page (#30)

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -9,16 +9,15 @@ import {BuildTrip} from './pages/Main/App';
 import Login from './pages/Login/App';
 import {RecommendPage} from './pages/recommendationpage/App';
 import {AboutUsPage} from './pages/AboutUs/App';
-// import LoginPage from './pages/Login';
+import {HomePage} from './pages/Home/App';
 
 const Main = () => {
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-        <Route exact path='/' component={Login}></Route>
+        <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/build' component={BuildTrip}></Route>
         <Route exact path='/recommend' component={RecommendPage}></Route>
         <Route exact path='/about-us' component={AboutUsPage}></Route>
-      {/*<Route exact path='/signup' component={Signup}></Route>*/}
     </Switch>
   );
 }
